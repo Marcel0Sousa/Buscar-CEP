599af7bf86cbfb8691c89587ff7f1ba890e9813c
Marcel0Sousa <tchelospt@gmail.com>
2025-05-27 06:00:24 -0300

    App of Layout

--- app/src/main/java/com/marcelo/buscarcep/MainActivity.kt
+++ app/src/main/java/com/marcelo/buscarcep/MainActivity.kt
@@ -4,23 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.marcelo.buscarcep.ui.theme.BuscarCEPTheme
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.marcelo.buscarcep.ui.theme.view.SearchCep
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BuscarCEPTheme {
 
-            }
+                val navController: NavHostController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "searchCep"
+                ) {
+                    composable("searchCep") {
+                        SearchCep(navController = navController)
+                    }
+                }
         }
     }
 }
